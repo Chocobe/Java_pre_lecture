c3b1c501e11cb8eceb9534d53b0a8a76c9789e34
Chocobe <kyw05171@gmail.com>
2019-05-18 14:51:19 +0900

    <master> : Created ex14

---
+++ java_pre_lecture/src/ex014/DoWhileSample.java
@@ -0,0 +1,14 @@
+package ex014;
+
+public class DoWhileSample {
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		
+		do {
+			System.out.print(c + ", ");
+			c = (char)(c + 1);
+			
+		} while(c <= 'z');		
+	}
+}
