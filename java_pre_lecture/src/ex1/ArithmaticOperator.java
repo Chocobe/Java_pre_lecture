6ba02e42455743933c2a7c920b9745c5002605df
Chocobe <kyw05171@gmail.com>
2019-05-18 13:01:05 +0900

    <master> : Created ex1

---
+++ java_pre_lecture/src/ex1/ArithmaticOperator.java
@@ -0,0 +1,25 @@
+// JAVA 선행수업 2번째 시간
+
+package ex1;
+
+import java.util.Scanner;
+
+public class ArithmaticOperator {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("정수(초)를 입력하세요 : " );
+		
+		int time = scanner.nextInt();
+		int second = time % 60;
+		int minute = (time / 60) % 60;
+		int hour = (time / 60) / 60;
+		
+		System.out.print(time + "초는 ");
+		System.out.print(hour + "시간, ");
+		System.out.print(minute + "분, ");
+		System.out.println(second + "초입니다.");
+		
+		scanner.close();
+	}
+}
